4a05c0da8100bdfe0898a3fdb814decf8a760a4e
yuchen <songyuchen2003@hotmail.com>
2025-04-15 20:15:06 +0200

    stop the application when the window closed

--- src/index.py
+++ src/index.py
@@ -5,15 +5,31 @@ from ui.expenses_view import ExpensesView
 class MoneyTrackApp(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.withdraw()  # directly display the login interface
+        self.withdraw()
+
+        self.login_window = None
+        self.expenses_window = None
+
         self.show_login()
 
     def show_login(self):
-        LoginView(self, on_login_success=self.on_login_success)
+        self.login_window = LoginView(self, on_login_success=self.on_login_success)
+        self.login_window.protocol("WM_DELETE_WINDOW", self.on_login_close)
+
+    def on_login_close(self):
+        self.login_window.destroy()
+        self.destroy()
 
     def on_login_success(self, username):
         print(f"User {username} logged in!")
-        ExpensesView(self, username)
+        self.login_window.destroy()
+        
+        self.expenses_window = ExpensesView(self, username)
+        self.expenses_window.protocol("WM_DELETE_WINDOW", self.on_expenses_close)
+
+    def on_expenses_close(self):
+        self.expenses_window.destroy()
+        self.destroy() 
 
 if __name__ == "__main__":
     app = MoneyTrackApp()
